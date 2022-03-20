0234181646f19900102ca58ce93ae2027e36008a
IFEE09 <ivanfaridespadas@gmail.com>
2022-03-20 12:21:43 -0600

    agregando la base de datos y creando la tabla

---
+++ data/migracion.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE tutorial_crud;
+
+use tutorial_crud;
+
+CREATE TABLE alumnos (
+
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(30) NOT NULL,
+    apellido VARCHAR(30) NOT NULL,
+    email VARCHAR(30) NOT NULL,
+    edad INT(3),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
